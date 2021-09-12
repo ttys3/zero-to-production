55ef74015c8a0a863bb916c32cb6417773847c70
Luca Palmieri <lpalmieri@truelayer.com>
2021-09-12 13:01:29 +0200

    Fix title.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,8 @@
+use crate::auth::{validate_credentials, Credentials};
 use actix_web::http::header::LOCATION;
 use actix_web::web;
 use actix_web::HttpResponse;
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -8,7 +10,17 @@ pub struct FormData {
     password: String,
 }
 
-pub async fn submit_login(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn submit_login(pool: web::Data<PgPool>, form: web::Form<FormData>) -> HttpResponse {
+    let form = form.0;
+    let _user_id = validate_credentials(
+        Credentials {
+            username: form.username,
+            password: form.password,
+        },
+        &pool,
+    )
+    .await
+    .unwrap_or_default();
     HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
         .finish()
