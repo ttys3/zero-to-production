6e172751a5dae2c70da76153f0d7d5d1ec5fdc88
Luca Palmieri <lpalmieri@truelayer.com>
2021-09-27 15:23:15 +0200

    Checkpoint.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,13 @@
 use actix_web::http::header::ContentType;
+use actix_web::web;
 use actix_web::HttpResponse;
+use tera::{Context, Tera};
 
-pub async fn login_form() -> HttpResponse {
+pub async fn login_form(templates: web::Data<Tera>) -> HttpResponse {
+    let body = templates
+        .render("login/login.html", &Context::new())
+        .unwrap();
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("login.html"))
+        .body(body)
 }
